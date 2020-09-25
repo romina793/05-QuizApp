bc5dcb09bafb192eddf4bd6b282d3355c4e0e17b
romina.pozzuto <romina.pozzuto@naranjax.com>
2020-09-25 14:36:54 -0300

    init commit

---
+++ 05-QuizApp/Controller/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  05-QuizApp
+//
+//  Created by Romina Pozzuto on 25/09/2020.
+//  Copyright © 2020 Romina Pozzuto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 05-QuizApp/Extension/Extension.swift
@@ -0,0 +1,42 @@
+//
+//  Extension.swift
+//  05-QuizApp
+//
+//  Created by Romina Pozzuto on 25/09/2020.
+//  Copyright © 2020 Romina Pozzuto. All rights reserved.
+//
+
+
+import UIKit
+
+fileprivate extension UIButton{
+    
+    @IBInspectable var borderWidth: CGFloat{
+        set{
+            layer.borderWidth = newValue
+        }
+        get{
+            return layer.borderWidth
+        }
+    }
+    
+    @IBInspectable var cornerRadius: CGFloat{
+        set{
+            layer.cornerRadius = newValue
+        }
+        get{
+            return layer.cornerRadius
+        }
+    }
+    
+    @IBInspectable var borderColor: UIColor?{
+        set{
+            guard let uiColor = newValue else {return}
+            layer.borderColor = uiColor.cgColor
+        }
+        get{
+            guard let color = layer.borderColor else {return nil}
+            return UIColor(cgColor: color)
+        }
+    }
+}
